8b2801a7f8bd28a51e6297887dc0bdd6230cad27
Stefan Siegl <stefan.siegl@tech11.com>
2025-02-15 16:48:39 +0100

    parse agent args + write json to file

--- agent/src/main/java/de/brokenpipe/dojo/undercovered/agent/UndercoveredAgent.java
+++ agent/src/main/java/de/brokenpipe/dojo/undercovered/agent/UndercoveredAgent.java
@@ -1,6 +1,10 @@
 package de.brokenpipe.dojo.undercovered.agent;
 
 import java.lang.instrument.Instrumentation;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.brokenpipe.dojo.undercovered.coverista.tracking.Tracker;
@@ -12,26 +16,49 @@ public class UndercoveredAgent {
 
 	public static void premain(final String agentArgs, final Instrumentation inst) {
 
-		log.info("[Agent] In premain method");
-		final var collector = Tracker.createCollector(null, null);
+		log.info("[Agent] In premain method, agentArgs: " + agentArgs);
+		final var args = AgentArgs.fromString(agentArgs);
 
-		inst.addTransformer(new UndercoverTransformer(collector));
+		final var collector = Tracker.createCollector(args.includes, args.excludes);
+
+		final UndercoverTransformer transformer = new UndercoverTransformer(collector);
+		inst.addTransformer(transformer);
 
 		Runtime.getRuntime().addShutdownHook(
 				new Thread(() -> {
 					System.out.println("In the middle of a shutdown");
-					serializeCollectorToJson(collector);
+					inst.removeTransformer(transformer);
+
+					if (args.destfile != null) {
+						serializeCollectorToJson(collector, args.destfile);
+					}
 				})
 		);
 	}
 
-	private static void serializeCollectorToJson(final TrackingCollector collector) {
+	private static void serializeCollectorToJson(final TrackingCollector collector, final String destfile) {
 		final var objectMapper = new ObjectMapper();
-		try {
-			final String json = objectMapper.writeValueAsString(collector);
-			System.out.println("Serialized collector to JSON: " + json);
+
+		try (final var writer = new java.io.FileWriter(destfile)) {
+			objectMapper.writeValue(writer, collector);
 		} catch (final Exception e) {
 			log.severe("Unable to serialize collector to JSON:" + e);
+		}
+	}
+
+	private record AgentArgs(String destfile, Set<String> includes, Set<String> excludes) {
+		public static AgentArgs fromString(final String agentArgs) {
+			final Map<String, String> argsMap = Arrays.stream(agentArgs.split(","))
+					.map(s -> s.split("="))
+					.collect(Collectors.toMap(a -> a[0], a -> a[1]));
+
+			final String destfile = argsMap.get("destfile");
+			final Set<String> includes = argsMap.containsKey("includes") ?
+					Set.of(argsMap.get("includes").split(":")) : Set.of();
+			final Set<String> excludes = argsMap.containsKey("excludes") ?
+					Set.of(argsMap.get("excludes").split(":")) : Set.of();
+
+			return new AgentArgs(destfile, includes, excludes);
 		}
 	}
 }
